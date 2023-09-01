f4476cbe81418cd786fb5e29cbe9b345cefc64dd
Souvik Chakraborty <Souvikc3030@gmail.com>
2023-09-01 22:03:35 +0530

    gitignore

--- sms/main.py
+++ sms/main.py
@@ -1,12 +1,10 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
-
+from data import *
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-TWILIO_AUTH_TOKEN = "aa876b3bcdae2dac3d1e15ef3536f508"
-TWILIO_ACCOUNT_SID = "ACea175db3e75bfcf637826dbf2a379b09"
 
 account_sid = os.environ[TWILIO_ACCOUNT_SID]
 auth_token = os.environ[TWILIO_AUTH_TOKEN]
